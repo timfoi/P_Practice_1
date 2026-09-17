5c030a406a77443202462fd00ec9ce5185abcf98
m0trex <timofeyalisov@gmail.com>
2026-09-17 22:43:32 +0300

    add namespace

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
 #include <chrono>
-class Clicker
+namespace mtt
 {
-public:
-  Clicker():
-    start_(std::chrono::high_resolution_clock::now())
-  {}
-  double millisec() const
+  class Clicker
   {
-    using std::chrono::duration_cast;
-    using std::chrono::high_resolution_clock;
-    using std::chrono::milliseconds;
-    auto t = high_resolution_clock::now();
-    return static_cast< double >(duration_cast< milliseconds >(t - start_).count());
-  }
+  public:
+    Clicker():
+      start_(std::chrono::high_resolution_clock::now())
+    {}
+    double millisec() const
+    {
+      using std::chrono::duration_cast;
+      using std::chrono::high_resolution_clock;
+      using std::chrono::milliseconds;
+      auto t = high_resolution_clock::now();
+      return static_cast< double >(duration_cast< milliseconds >(t - start_).count());
+    }
 
-private:
-  std::chrono::high_resolution_clock::time_point start_;
-};
+  private:
+    std::chrono::high_resolution_clock::time_point start_;
+  };
+}
