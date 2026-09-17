4d0114b72f115b6fd57bb0cb9acb510162d6e3e2
m0trex <timofeyalisov@gmail.com>
2026-09-17 23:42:42 +0300

    working version

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <cerrno>
 #include <chrono>
 #include <cstdlib>
+#include <future>
 #include <iostream>
-#include <limits>
 #include <vector>
 
 namespace mtt
@@ -10,25 +10,18 @@ namespace mtt
   class Clicker
   {
   public:
-    Clicker():
-      start_(std::chrono::high_resolution_clock::now())
-    {}
-    double millisec() const
-    {
-      using std::chrono::duration_cast;
-      using std::chrono::high_resolution_clock;
-      using std::chrono::milliseconds;
-      auto t = high_resolution_clock::now();
-      return static_cast< double >(duration_cast< milliseconds >(t - start_).count());
-    }
+    Clicker();
+    double millisec() const;
 
   private:
     std::chrono::high_resolution_clock::time_point start_;
   };
-}
 
-using data_t = std::vector< unsigned long long >;
-using value_t = data_t::value_type;
+  using data_t = std::vector< unsigned long long >;
+  using value_t = data_t::value_type;
+
+  value_t sum(data_t::const_iterator start, data_t::const_iterator end);
+}
 
 int main(int argc, char *argv[])
 {
@@ -39,27 +32,68 @@ int main(int argc, char *argv[])
 
   char *endptr = nullptr;
   errno = 0;
-  long val = std::strtoul(argv[1], &endptr, 10);
 
-  using limit_t = std::numeric_limits< int >;
+  long val = std::strtol(argv[1], &endptr, 10);
 
-  if (endptr == argv[1] || *endptr != '\0' || errno == ERANGE || val < limit_t::min() || val > limit_t::max()) {
-    std::cerr << "Incorect argument" << '\n';
+  if (endptr == argv[1] || *endptr != '\0' || errno == ERANGE || val < 1) {
+    std::cerr << "Incorrect argument" << '\n';
     return 2;
   }
 
-  int n = static_cast< int >(val);
+  size_t threads = static_cast< size_t >(val);
 
   constexpr size_t size{1'000'000'000};
+  mtt::data_t values(size, 1);
+  mtt::value_t sum{0};
+
   double init{0}, total{0};
-  value_t sum{0};
-  {
-    mtt::Clicker cl;
-    data_t values(size, 1);
-    init = cl.millisec();
-    for (size_t i = 0; i < values.size(); ++i) {
-      sum += values[i];
-    }
-    total = cl.millisec();
+  mtt::Clicker cl;
+
+  std::vector< std::future< mtt::value_t > > results;
+  results.reserve(threads);
+  size_t base = size / threads;
+  size_t remainder = size % threads;
+  auto chunkBegin = values.cbegin();
+  using diff_t = mtt::data_t::difference_type;
+
+  init = cl.millisec();
+
+  for (size_t i = 0; i < threads; ++i) {
+    size_t chunkSize = base + (i < remainder ? 1 : 0);
+    auto chunkEnd = chunkBegin + static_cast< diff_t >(chunkSize);
+
+    results.emplace_back(std::async(std::launch::async, mtt::sum, chunkBegin, chunkEnd));
+    chunkBegin = chunkEnd;
   }
+
+  for (auto &ft : results) {
+    sum += ft.get();
+  }
+
+  total = cl.millisec();
+
+  std::cout << "Res: " << sum << '\n';
+  std::cout << "Time: " << total - init << '\n';
+}
+
+mtt::Clicker::Clicker():
+  start_(std::chrono::high_resolution_clock::now())
+{}
+
+double mtt::Clicker::millisec() const
+{
+  using std::chrono::duration_cast;
+  using std::chrono::high_resolution_clock;
+  using std::chrono::milliseconds;
+  auto t = high_resolution_clock::now();
+  return static_cast< double >(duration_cast< milliseconds >(t - start_).count());
+}
+
+mtt::value_t mtt::sum(data_t::const_iterator start, data_t::const_iterator end)
+{
+  value_t res = 0;
+  for (; start != end; ++start) {
+    res += *start;
+  }
+  return res;
 }
